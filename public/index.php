5d09579b97c72cb42506a1bc4554480240e4b71e
Jimmy Parker <jimmy.tdh@gmail.com>
2024-10-09 16:49:22 +0800

    add .env.dms

--- public/index.php
+++ public/index.php
@@ -46,6 +46,7 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+$app->loadEnvironmentFrom('.env.dms');
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
